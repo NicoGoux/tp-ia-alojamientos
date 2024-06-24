05ab01eefef9d24faaea42d8de0f79c174efeca6
NicolásGoux <nicolasgoux2000@gmail.com>
2024-06-24 18:32:36 -0300

    agregado formato de cards

--- api/controller.js
+++ api/controller.js
@@ -2,7 +2,31 @@ import alojamientos from './alojamientos.js';
 
 const getAlojamientos = async (req, res) => {
 	console.log(req);
-	return res.json({ results: alojamientos });
+	const cards = alojamientos.map(createCard);
+	return res.json(cards);
+};
+
+const createCard = (alojamiento) => {
+	return {
+		title: alojamiento.nombre,
+		description: `
+        Ciudad: ${alojamiento.ciudad}
+        Tipo de Alojamiento: ${alojamiento.tipoAlojamiento}
+        Ubicación: ${alojamiento.ubicacion}
+        Facilidades: ${alojamiento.facilidades.join(', ')}
+        `,
+		buttons: [
+			{
+				text: 'Más detalles',
+				url: '#',
+			},
+		],
+	};
+};
+
+return {
+	response_type: 'carousel',
+	cards: cards,
 };
 
 export { getAlojamientos };
