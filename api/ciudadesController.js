e4ec6cc3bf0e24d593d2cfe64bae4b48c15afa4c
NicolásGoux <nicolasgoux2000@gmail.com>
2024-06-27 18:00:11 -0300

    modificada ubicacion

--- api/ciudadesController.js
+++ api/ciudadesController.js
@@ -1,6 +1,13 @@
 const getCiudades = async (req, res) => {
 	return res.json({
-		result: ['Buenos Aires', 'Cordoba', 'Rosario', 'Santa Fe'],
+		result: [
+			'Buenos Aires',
+			'Cordoba',
+			'Rosario',
+			'Santa Fe',
+			'Mar del Plata',
+			'Pinamar',
+		],
 	});
 };
 
