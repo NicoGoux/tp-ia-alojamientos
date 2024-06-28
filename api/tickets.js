dedfdb07947ccd5d382dc4cf3912e36495058ce6
NicolásGoux <nicolasgoux2000@gmail.com>
2024-06-28 18:45:02 -0300

    agregado numero de reserva

--- api/tickets.js
+++ api/tickets.js
@@ -4,61 +4,72 @@ const tickets = [
 		tipoTicket: 'Reserva',
 		descripcion:
 			'Reservé un departamento en Buenos Aires pero al llegar el alojamiento no estaba disponible.',
+		numeroReserva: 1,
 	},
 	{
 		numeroTicket: 2,
 		tipoTicket: 'Alojamiento',
 		descripcion:
 			'El hotel en Santa Fe no tiene las facilidades prometidas como el desayuno incluido.',
+		numeroReserva: 2,
 	},
 	{
 		numeroTicket: 3,
 		tipoTicket: 'Discrepancias con la descripción',
 		descripcion:
 			'La cabaña en Rosario mencionaba tener aire acondicionado pero no funciona correctamente.',
+		numeroReserva: 3,
 	},
 	{
 		numeroTicket: 4,
 		tipoTicket: 'Pago',
 		descripcion:
 			'Pagué por una casa en San Telmo pero el sistema no registró mi pago correctamente.',
+		numeroReserva: 4,
 	},
 	{
 		numeroTicket: 5,
 		tipoTicket: 'Atención',
 		descripcion:
 			'El servicio al cliente no respondió a mis correos sobre un problema con la quinta en Tigre.',
+		numeroReserva: 5,
 	},
 	{
 		numeroTicket: 6,
 		tipoTicket: 'Otro',
 		descripcion:
 			'Tengo un problema con la ubicación del alojamiento en Córdoba, no está en la zona que esperaba.',
+		numeroReserva: 6,
 	},
 	{
 		numeroTicket: 7,
 		tipoTicket: 'Alojamiento',
 		descripcion:
 			'El departamento en Nueva Córdoba no estaba limpio al momento de mi llegada.',
+		numeroReserva: 7,
 	},
 	{
 		numeroTicket: 8,
 		tipoTicket: 'Discrepancias con la descripción',
 		descripcion:
 			'La casa quinta en Santa Fe no tiene acceso a la pileta como se anunciaba.',
+		numeroReserva: 8,
 	},
 	{
 		numeroTicket: 9,
 		tipoTicket: 'Pago',
 		descripcion:
 			'Realicé el pago por adelantado pero el hotel en Rosario no tenía mi reserva registrada.',
+		numeroReserva: 9,
 	},
 	{
 		numeroTicket: 10,
 		tipoTicket: 'Atención',
 		descripcion:
 			'Necesito ayuda con las direcciones para llegar al hotel en Buenos Aires, la información en la reserva no es clara.',
+		numeroReserva: 10,
 	},
 ];
 
 export default tickets;
+
